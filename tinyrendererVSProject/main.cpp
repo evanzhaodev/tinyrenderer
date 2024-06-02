f9d6f72dc28c866ca089d2ef3794a3384bad3079
Evan Zhao <hzhao5@uw.edu>
2024-06-02 15:43:05 -0700

    Add triangle rasterization (line sweep)

--- tinyrendererVSProject/main.cpp
+++ tinyrendererVSProject/main.cpp
@@ -4,6 +4,7 @@
 
 const TGAColor white = TGAColor{ 255, 255, 255, 255 };
 const TGAColor red = TGAColor{ 0, 0, 255, 255 };
+const TGAColor green = TGAColor{ 0, 255, 0, 255 };
 
 void line(int x0, int y0, int x1, int y1, TGAImage& image, const TGAColor& color) {
     bool steep = false;
@@ -33,6 +34,36 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image, const TGAColor& color
     }
 }
 
+float Vec2Distance(Vec2i v0, Vec2i v1) {
+    return std::sqrtf((v1.x - v0.x) * (v1.x - v0.x) + (v1.y - v0.y) * (v1.y - v0.y));
+}
+
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, const TGAColor& color) {
+    if (t0.x > t1.x) {
+        std::swap(t1, t0);
+    }
+    if (t0.x > t2.x) {
+        std::swap(t2, t0);
+    }
+    if (t1.x > t2.x) {
+        std::swap(t2, t1);
+    }
+
+    for (int x = t0.x; x <= t1.x; x++) {
+        int y1 = t0.y + 1.f * (x - t0.x) * (t1.y - t0.y) / (t1.x - t0.x);
+        int y2 = t0.y + 1.f * (x - t0.x) * (t2.y - t0.y) / (t2.x - t0.x);
+
+        line(x, y1, x, y2, image, color);
+    }
+
+    for (int x = t2.x; x >= t1.x; x--) {
+        int y1 = t2.y - 1.f * (t2.x - x) * (t2.y - t1.y) / (t2.x - t1.x);
+        int y2 = t2.y - 1.f * (t2.x - x) * (t2.y - t0.y) / (t2.x - t0.x);
+
+        line(x, y1, x, y2, image, color);
+    }
+}
+
 int main(int argc, char** argv) {
     Model* model = NULL;
     const int width = 800;
@@ -46,7 +77,7 @@ int main(int argc, char** argv) {
     }
 
     TGAImage image(width, height, TGAImage::RGB);
-    for (int i = 0; i < model->nfaces(); i++) {
+    /*for (int i = 0; i < model->nfaces(); i++) {
         std::vector<int> face = model->face(i);
         for (int j = 0; j < 3; j++) {
             Vec3f v0 = model->vert(face[j]);
@@ -57,7 +88,14 @@ int main(int argc, char** argv) {
             int y1 = (v1.y + 1.) * height / 2.;
             line(x0, y0, x1, y1, image, white);
         }
-    }
+    }*/
+
+    Vec2i t0[3] = { Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80) };
+    Vec2i t1[3] = { Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180) };
+    Vec2i t2[3] = { Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180) };
+    triangle(t0[0], t0[1], t0[2], image, red);
+    triangle(t1[0], t1[1], t1[2], image, white);
+    triangle(t2[0], t2[1], t2[2], image, green);
 
     image.write_tga_file("output.tga");
     delete model;
